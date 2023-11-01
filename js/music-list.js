27192e444b234f10b7b5f554ae03c4e9d06c658c
huyvmu <huyhtht135@gmail.com>
2023-11-01 20:17:48 +0700

    add

---
+++ js/music-list.js
@@ -0,0 +1,58 @@
+let allMusic =[
+    {
+        name: "Em Của Ngày Hôm Qua",
+        artist: "Sơn Tùng M-TP",
+        img:"Pic\EmCuaNgayHomQua.jpg",
+        src:"js\EmCuaNgayHomQua-SonTungMTP.mp3",
+    },
+    {
+        name: "Chắc Ai Đó Sẽ Về",
+        artist: "Sơn Tùng M-TP",
+        img:"Pic\ChacAiDoSeVe.jpg",
+        src:"js\ChacAiDoSeVe.mp3",
+    },
+    {
+        name: "Em Đừng Đi",
+        artist: "Sơn Tùng M-TP",
+        img:"Pic\EmDungDi.jpg",
+        src:"js\EmDungDi.mp3",
+    },
+    {
+        name: "Khuôn Mặt Đáng Thương",
+        artist: "Sơn Tùng M-TP",
+        img:"Pic\KhuonMatDangThuong.jpg",
+        src:"js\KhuonMatDangThuong.pm3",
+    }
+    // 
+    ,
+    {
+        name: "Em Của Ngày Hôm Qua",
+        artist: "Sơn Tùng M-TP",
+        img:"Pic\EmCuaNgayHomQua.jpg",
+        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
+    },
+    {
+        name: "Em Của Ngày Hôm Qua",
+        artist: "Sơn Tùng M-TP",
+        img:"Pic\EmCuaNgayHomQua.jpg",
+        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
+    },
+    {
+        name: "Em Của Ngày Hôm Qua",
+        artist: "Sơn Tùng M-TP",
+        img:"Pic\EmCuaNgayHomQua.jpg",
+        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
+    },
+    {
+        name: "Em Của Ngày Hôm Qua",
+        artist: "Sơn Tùng M-TP",
+        img:"Pic\EmCuaNgayHomQua.jpg",
+        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
+    },
+    {
+        name: "Em Của Ngày Hôm Qua",
+        artist: "Sơn Tùng M-TP",
+        img:"Pic\EmCuaNgayHomQua.jpg",
+        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
+    }
+]
