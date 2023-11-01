73bb1ffb2cc1cd3ac34298056b933a9b62b099ef
huyvmu <huyhtht135@gmail.com>
2023-11-01 21:42:29 +0700

    ok

--- js/music-list.js
+++ js/music-list.js
@@ -2,57 +2,25 @@ let allMusic =[
     {
         name: "Em Của Ngày Hôm Qua",
         artist: "Sơn Tùng M-TP",
-        img:"Pic\EmCuaNgayHomQua.jpg",
-        src:"js\EmCuaNgayHomQua-SonTungMTP.mp3",
+        img:"EmCuaNgayHomQua1",
+        src:"EmCuaNgayHomQua",
     },
     {
         name: "Chắc Ai Đó Sẽ Về",
         artist: "Sơn Tùng M-TP",
-        img:"Pic\ChacAiDoSeVe.jpg",
-        src:"js\ChacAiDoSeVe.mp3",
+        img:"ChacAiDoSeVe",
+        src:"ChacAiDoSeVe",
     },
     {
         name: "Em Đừng Đi",
         artist: "Sơn Tùng M-TP",
-        img:"Pic\EmDungDi.jpg",
-        src:"js\EmDungDi.mp3",
+        img:"EmDungDi",
+        src:"EmDungDi",
     },
     {
         name: "Khuôn Mặt Đáng Thương",
         artist: "Sơn Tùng M-TP",
-        img:"Pic\KhuonMatDangThuong.jpg",
-        src:"js\KhuonMatDangThuong.pm3",
-    }
-    // 
-    ,
-    {
-        name: "Em Của Ngày Hôm Qua",
-        artist: "Sơn Tùng M-TP",
-        img:"Pic\EmCuaNgayHomQua.jpg",
-        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
-    },
-    {
-        name: "Em Của Ngày Hôm Qua",
-        artist: "Sơn Tùng M-TP",
-        img:"Pic\EmCuaNgayHomQua.jpg",
-        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
-    },
-    {
-        name: "Em Của Ngày Hôm Qua",
-        artist: "Sơn Tùng M-TP",
-        img:"Pic\EmCuaNgayHomQua.jpg",
-        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
-    },
-    {
-        name: "Em Của Ngày Hôm Qua",
-        artist: "Sơn Tùng M-TP",
-        img:"Pic\EmCuaNgayHomQua.jpg",
-        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
-    },
-    {
-        name: "Em Của Ngày Hôm Qua",
-        artist: "Sơn Tùng M-TP",
-        img:"Pic\EmCuaNgayHomQua.jpg",
-        src:"js\EmCuaNgayHomQua-SonTungMTP-3295488.mp3",
+        img:"KhuonMatDangThuong",
+        src:"KhuonMatDangThuong",
     }
 ]
---
+++ js/script.js
@@ -0,0 +1,43 @@
+// let's select all required tag or elements
+
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+mainAudio = wrapper.querySelector("#main-audio"),
+playPauseBtn = wrapper.querySelector(".play-pause");
+
+let musicIndex = 2;
+
+window.addEventListener("load", () =>{
+    loadMusic(musicIndex); // calling load music once window loaded
+})
+
+// load music function
+function loadMusic(indexNumb){
+    musicName.innerText = allMusic[indexNumb - 1].name;
+    musicArtist.innerText = allMusic[indexNumb - 1].artist;
+    musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
+    mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+}
+
+// play music function
+function playMusic(){
+    wrapper.classList.add("paused");
+    playPauseBtn.querySelector("i").innerText = "pause";
+    mainAudio.play()
+}
+
+// pause music function
+function pauseMusic(){
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause()
+}
+
+// play or music button event
+playPauseBtn.addEventListener("click", ()=>{
+    const isMusicPaused = wrapper.classList.contains("paused");
+    // if isMusicPaused is true then call pauseMusic else call playMusic
+    isMusicPaused ? pauseMusic() : playMusic();
+});
